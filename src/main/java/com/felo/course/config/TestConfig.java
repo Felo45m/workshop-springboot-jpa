449caa2a97984c25f68e61fc8c1709a061f9a5b9
Felo45m <felipemelo713@gmail.com>
2023-04-05 10:41:14 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/felo/course/config/TestConfig.java
+++ src/main/java/com/felo/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.felo.course.entities.Category;
 import com.felo.course.entities.Order;
+import com.felo.course.entities.OrderItem;
 import com.felo.course.entities.Product;
 import com.felo.course.entities.User;
 import com.felo.course.entities.enums.OrderStatus;
 import com.felo.course.repositories.CategoryRepository;
+import com.felo.course.repositories.OrderItemRepository;
 import com.felo.course.repositories.OrderRepository;
 import com.felo.course.repositories.ProductRepository;
 import com.felo.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -68,8 +73,13 @@ public class TestConfig implements CommandLineRunner{
 					
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
 	}
-	
-	
-	
 }
---
+++ src/main/java/com/felo/course/repositories/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.felo.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.felo.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+	
+}
