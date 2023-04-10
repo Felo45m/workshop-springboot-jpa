a5cb255d03524c3e9456f1358a88241bee13229e
Felo45m <felipemelo713@gmail.com>
2023-04-10 15:04:41 -0300

    Exception handling - findById

--- src/main/java/com/felo/course/services/UserService.java
+++ src/main/java/com/felo/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.felo.course.entities.User;
 import com.felo.course.repositories.UserRepository;
+import com.felo.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
